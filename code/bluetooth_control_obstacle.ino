ae33ed52517c0335a1508d402dbb05cc0b2183bc
ayushyalasangi <ayushyalasangi@gmail.com>
2026-02-09 22:46:34 +0530

    Update bluetooth_control_obstacle.ino

--- code/bluetooth_control_obstacle.ino
+++ code/bluetooth_control_obstacle.ino
@@ -85,40 +85,6 @@ void Obstacle() {
 
 }
 
-void voicecontrol() {
-  if (Serial.available() > 0) {
-    value = Serial.read();
-    Serial.println(value);
-    if (value == '^') {
-      forward();
-    } else if (value == '-') {
-      backward();
-    } else if (value == '<') {
-      L = leftsee();
-      servo.write(spoint);
-      if (L >= 10 ) {
-        left();
-        delay(500);
-        Stop();
-      } else if (L < 10) {
-        Stop();
-      }
-    } else if (value == '>') {
-      R = rightsee();
-      servo.write(spoint);
-      if (R >= 10 ) {
-        right();
-        delay(500);
-        Stop();
-      } else if (R < 10) {
-        Stop();
-      }
-    } else if (value == '*') {
-      Stop();
-    }
-  }
-}
-
 int ultrasonic() {
   digitalWrite(Trig, LOW);
   delayMicroseconds(4);
